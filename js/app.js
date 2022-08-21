67c0517f684b064cf5be3e05fe6435ae361f0863
Amanpreet1199 <kauramanpreet1199@gmail.com>
2022-08-21 13:34:09 +0530

    Change layout and dlt exceess files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 function go() {
-	window.open("https://www.smartr.me/me/amanpreet.kaur232", "_blank");
+	window.open(
+		"https://www.smartr.me/api/public/profiles/amanpreet.kaur232/localized-resume",
+		"_blank"
+	);
 	// location.href = "https://www.smartr.me/me/amanpreet.kaur232";
 }
